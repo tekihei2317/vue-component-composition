54d5fd5df82222cff28c83f706bff1e0d93cbf69
tekihei2317 <tekihei2317@gmail.com>
2021-10-03 20:01:23 +0900

    refactor: タイマーの計算のロジックを移動 & script setupに変更

--- src/samples/04_time/TimeViewAfter.vue
+++ src/samples/04_time/TimeViewAfter.vue
@@ -1,25 +1,9 @@
+<script lang="ts" setup>
+import { useTimer } from './useTimer'
+
+const { timeText } = useTimer()
+</script>
+
 <template>
   <div class="TimeView">{{ timeText }}</div>
 </template>
-
-<script lang="ts">
-import { computed, defineComponent, ref } from 'vue'
-import { useInterval } from './useInterval'
-
-export default defineComponent({
-  setup() {
-    const date = ref(new Date())
-
-    const timeText = computed(() => {
-      const d = date.value
-      return [d.getHours(), d.getMinutes(), d.getSeconds()].map((n) => n.toString().padStart(2, '0')).join(':')
-    })
-
-    useInterval(() => {
-      date.value = new Date()
-    }, 100)
-
-    return { timeText }
-  },
-})
-</script>
