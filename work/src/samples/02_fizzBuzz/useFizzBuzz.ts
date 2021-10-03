9b5787a1e7f53d1d1f2a40f6e0d7f945daf58725
tekihei2317 <tekihei2317@gmail.com>
2021-10-03 12:01:40 +0900

    feat: FizzBuzzのロジックをcomposition functionに分割

--- work/src/samples/02_fizzBuzz/useFizzBuzz.ts
+++ work/src/samples/02_fizzBuzz/useFizzBuzz.ts
@@ -1,19 +1,20 @@
-import { computed, ref } from 'vue'
+import { ref, computed } from 'vue'
 
 export const useFizzBuzz = () => {
   const count = ref(1)
   const result = computed(() => {
-    if (count.value % 15 === 0) {
-      return 'FizzBuzz'
-    }
-    if (count.value % 5 === 0) {
-      return 'Buzz'
-    }
-    if (count.value % 3 === 0) {
-      return 'Fizz'
-    }
-    return String(count.value)
+    if (count.value % 15 === 0) return 'FizzBuzz'
+    if (count.value % 5 === 0) return 'Buzz'
+    if (count.value % 3 === 0) return 'Fizz'
+    return count.value
   })
 
-  return { count, result }
+  const up = () => {
+    count.value++
+  }
+  const reset = () => {
+    count.value = 1
+  }
+
+  return { count, result, up, reset }
 }
